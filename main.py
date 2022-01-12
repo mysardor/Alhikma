8c3037266a70d097dea6c794b9da600aab35f2f8
mysardor <mysardorr@gmail.com>
2022-01-12 17:59:49 +0500

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+import telebot
+from telebot import types
 import sys
 from typing import Text
 from flask import Flask
@@ -19,203 +22,159 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 user_dict = {}
 
+
 class User:
-    def __init__(self, name, location = None, phone=None):
+    def __init__(self, name):
         self.name = name
-        self.location = location
-        self.phone = phone
+        self.age = None
+        self.sex = None
 
-      
 
-@bot.message_handler(commands=['start'])
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
 def start(message):
     if message.chat.type == "private":
-        name = message.from_user.first_name
-        keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-        button_info = types.KeyboardButton(text="ℹ️ Ma'lumot olish")
-        button_book = types.KeyboardButton(text="🛒 Buyurtma qilish")
-        keyboard.add(button_book,button_info)
-        bot.reply_to(message, name + " Kerakli bo'limni tanlang",parse_mode= "Markdown", reply_markup=keyboard)
-        
-@bot.message_handler(content_types=['text'])
-def userRequest(message):
-    if message.chat.type == "private":
-        if message.text == "ℹ️ Ma'lumot olish":
-            bot.send_message(message.chat.id, info_text)
-            start(message)
-        elif message.text == "🛒 Buyurtma qilish": 
-            bot.send_message(message.chat.id, "Ism-Familyangizni yozing")
-            bot.register_next_step_handler(message, process_name_step)
-        else:
-            bot.send_message(message.chat.id, "Bunday bo'lim mavjud emas")
-            start(message)
-      
+        msg = bot.reply_to(message, """Исм/Фамилия:""")
+        bot.register_next_step_handler(msg, process_name_step)
+
+
 def process_name_step(message):
-    if message.text == '/start':
-        userRequest(message)
-    else:
+    if message.chat.type == "private":
         try:
-            global userName
             chat_id = message.chat.id
+            global name
             name = message.text
-            userName = name
             user = User(name)
             user_dict[chat_id] = user
-            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-            button_phone = types.KeyboardButton(text="Telefon raqamni yuborish", request_contact=True)
-            keyboard.add(button_phone)
-            bot.send_message(message.chat.id, "Telefon raqamingizni yuboring", reply_markup=keyboard)
-            bot.register_next_step_handler(message, process_phone_step)
-        
+            msg = bot.reply_to(message, """Компаниянгиз номи:""")
+            bot.register_next_step_handler(msg, process_company_step)
         except Exception as e:
-            bot.reply_to(message, "Xatolik mavjud. Iltimos, qaytadan kiriting")
-        
-def process_phone_step(message):
-    try:
-        chat_id = message.chat.id
-        if message.content_type != 'contact' or message.text == '/start':
-            bot.reply_to(message, 'Xatolik mavjud. Iltimos, qaytadan kiriting')
-            bot.register_next_step_handler(message, process_phone_step)
-        user = user_dict[chat_id]
-        user.phone = message.text
-        phone = User(user.phone)
-        if message.content_type == 'contact':
-            global phone_num
-            phone_num = message.contact.phone_number
-            markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-            region1 = types.InlineKeyboardButton(text="Toshkent")
-            region2 = types.InlineKeyboardButton(text="Andijon")
-            region3 = types.InlineKeyboardButton(text="Farg'ona")
-            region3 = types.InlineKeyboardButton(text="Namangan")
-            region4 = types.InlineKeyboardButton(text="Sirdaryo")
-            region5 = types.InlineKeyboardButton(text="Jizzax")
-            region6 = types.InlineKeyboardButton(text="Qashqadaryo")
-            region7 = types.InlineKeyboardButton(text="Xorazm")
-            region8 = types.InlineKeyboardButton(text="Buxoro")
-            region9 = types.InlineKeyboardButton(text="Surhondaryo")
-            region10 = types.InlineKeyboardButton(text="Urganch")
-            region11 = types.InlineKeyboardButton(text="Navoiy")    
-            region12 = types.InlineKeyboardButton(text="Samarqand")
-            region13 = types.InlineKeyboardButton(text="Qoraqalpog'iston")
-            markup.add(region1,region2,region3,region4,region5,region6,region7,region8,region9,region10,region11,region12,region13)
-            bot.send_message(message.chat.id, "Qaysi viloyatdansiz ?", reply_markup=markup)
-            bot.register_next_step_handler(message, process_location_step)
-    except Exception as e:
-        bot.reply_to(message, "Xatolik mavjud. Iltimos, qaytadan kiriting")
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
 
-def process_location_step(message):
-    global province
-    province = message
-    try:
-        kb = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-        confirm = types.KeyboardButton(text="Ha")
-        reject = types.KeyboardButton(text="Yo'q")
-        kb.add(confirm, reject)
-        bot.send_message(message.chat.id, "Joylashuvingizni jo'natishni hohlaysizmi ?", reply_markup=kb)
-        bot.register_next_step_handler(message, process_userChoice_step)
-    except Exception as e:
-        bot.send_message(message.chat.id, e)
 
-def process_userChoice_step(message):
-    try:
-        if message.text == "Ha":
-            kb = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-            sendLocation = types.KeyboardButton(text="Joylashuvni jo'natish", request_location=True)
-            kb.add(sendLocation)
-            bot.reply_to(message, "Joylashuvingizni jo'nating", reply_markup=kb)
-            bot.register_next_step_handler(message, process_userLocation_step)
-        elif message.text == "Yo'q":
-            process_region_step(province)
-            global ul
-            ul = "jo'natishni hoxlamadi"
-        else:
-            bot.send_message(message, "Mavjud bo'lmagan buyruq.")
-            bot.register_next_step_handler(message, process_userChoice_step)
-            
-    except Exception as e:
-        bot.send_message(message.chat.id, "Xatolik mavjud. Iltimos, qaytadan kiriting")
-
-def process_userLocation_step(message):
-    if message.content_type == 'location':
+def process_company_step(message):
+    if message.chat.type == "private":
         try:
             chat_id = message.chat.id
-            location = message.text
-            process_region_step(province)
-            global latitude, longitude, ul
-            latitude = message.location.latitude
-            longitude = message.location.longitude
-            ul = "https://www.google.com/search?q=%s,%s"%(latitude,longitude)
-            print(ul)
+            global company
+            company = message.text
+            user = User(company)
+            user_dict[chat_id] = user
+            msg = bot.reply_to(message, """Лавозимингиз (агар ходим бўлсангиз):""")
+            bot.register_next_step_handler(msg, process_job_step)
         except Exception as e:
-            bot.send_message(message.chat.id, "Xatolik mavjud. Iltimos, qaytadan kiriting")
-    else:   
-        bot.send_message(message.chat.id, "Xatolik mavjud. Iltimos, qaytadan kiriting")
-        time.sleep(1)
-        process_location_step(province)
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
 
-def process_region_step(message):
+def process_job_step(message):
+    if message.chat.type == "private":
+        try:
+            chat_id = message.chat.id
+            global job
+            job = message.text
+            user = User(job)
+            user_dict[chat_id] = user
+            msg = bot.reply_to(message, """Компаниянгизнинг ўртача ойлик пул айланмаси (ихтиёрий):""")
+            bot.register_next_step_handler(msg, process_money_step)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
+
+def process_money_step(message):
+    if message.chat.type == "private":
+        try:
+            chat_id = message.chat.id
+            global money
+            money = message.text
+            user = User(money)
+            user_dict[chat_id] = user
+            msg = bot.reply_to(message, """Бизнесингизда қандай муаммолар учрамоқда:""")
+            bot.register_next_step_handler(msg, process_problem_step)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
+
+def process_problem_step(message):
+    if message.chat.type == "private":
+        try:
+            chat_id = message.chat.id
+            global problem
+            problem = message.text
+            user = User(problem)
+            user_dict[chat_id] = user
+            msg = bot.reply_to(message, """Бугунги тадбирдан кутувларингиз:""")
+            bot.register_next_step_handler(msg, process_wait_step)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
+
+def process_wait_step(message):
+    if message.chat.type == "private":
+        try:
+            chat_id = message.chat.id
+            global waitin
+            waitin = message.text
+            user = User(waitin)
+            user_dict[chat_id] = user
+            msg = bot.reply_to(message, """Спикерга саволларингиз:""")
+            bot.register_next_step_handler(msg, process_question_step)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
+
+def process_question_step(message):
+    if message.chat.type == "private":
+        try:
+            chat_id = message.chat.id
+            global question
+            question = message.text
+            user = User(question)
+            user_dict[chat_id] = user
+            msg = bot.reply_to(message, """Сиз билан бўгланишимиз учун телефон рақамингизни қолдиринг.""")
+            bot.register_next_step_handler(msg, process_phone_step)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
+
+def process_phone_step(message):
+    if message.chat.type == "private":
+        try:
+            chat_id = message.chat.id
+            global phoneNum
+            phoneNum = message.text
+            user = User(question)
+            user_dict[chat_id] = user
+            msg = bot.reply_to(message, """Ёзган маълумотларингиз тўгрилигини текширинг""")
+            markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
+            confirm = types.InlineKeyboardButton(text="Тасдиқлаш")
+            reject = types.InlineKeyboardButton(text="Қайта бошлаш")
+            markup.add(confirm, reject)
+            chat_id = message.chat.id
+            bot.send_message(chat_id, 'Исм-Фамилянгиз: ' + name + '\n Компаниянгиз номи: ' + company + '\n Лавозимингиз: ' + job + '\n Ойлик айланмангиз: ' + money + 
+            '\n Муаммоларингиз: ' + problem  + '\n Тадбирдан кутаётганингиз: ' + waitin  + '\n Телефон рақамингиз: ' + phoneNum , 
+            reply_markup=markup)
+            bot.register_next_step_handler(msg, process_confirm_step)
+        except Exception as e:
+            bot.reply_to(message, 'oooops, кутилмаган қарор!')
+
+def process_confirm_step(message):
     try:
         chat_id = message.chat.id
-        if message.content_type != 'text':
-            bot.reply_to(message, 'Xatolik mavjud. Iltimos, qaytadan kiriting')
-            bot.register_next_step_handler(message, process_phone_step)
+        confirm = message.text
         user = user_dict[chat_id]
-        user.location = message.text
-        location = User(user.location)
-        if message.content_type == 'text':
-            global userRegion
-            userRegion = message.text
-            markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True, one_time_keyboard=True)
-            confirm = types.InlineKeyboardButton(text="Tasdiqlash")
-            reject = types.InlineKeyboardButton(text="Bekor qilish")
-            markup.add(confirm, reject)
-            bot.send_message(message.chat.id, "Kiritilgan ma'lumotlaringiz tog'riligini tekshiring" + "\nIsmingiz: "
-                             + user.name + "\nViloyat: "+userRegion+"\nTelefon: "
-                             + phone_num +"\nMa'lumotlarni tasdiqlaysizmi ?", 
-                             reply_markup=markup)
-            bot.register_next_step_handler(message, process_confirm_step)
-    except Exception as e:
-        bot.reply_to(message, "Xatolik mavjud. Iltimos, qaytadan kiriting")
-        
-def process_confirm_step(message):
-    chat_id = message.chat.id
-    confirm = message.text
-    user = user_dict[chat_id]
-    if (confirm == u'Tasdiqlash'):
-        user.confirm = confirm
-        bot.send_message(message.chat.id, 'Rahmat, siz bilan imkon qadar tezroq aloqaga chiqamiz.')
-        bot.send_message(-1001508537723, '#Mijoz\n' + "Ismi: " + user.name + "\nUsername: @"
-                         +message.from_user.username+"\nViloyati: #"
-                         +userRegion+"\nTelefon raqami: "
-                         + phone_num +"\nManzil "+ul)
-        time.sleep(1)
-        start(message)
-    else:   
-        user_dict.clear()
-        msg = bot.send_message(message.chat.id, 'Bekor qilindi,\nIsm-Familyangizni qaytadan kiriting')
-        bot.register_next_step_handler(message, process_name_step)
+        if (confirm == u'Тасдиқлаш'):
+            user.confirm = confirm
+            bot.send_message(message.chat.id, 'Раҳмат, сиз билан имкон қадар тезроқ алоқага чиқамиз.')
+            bot.send_message(-1001595060544,'Исм-Фамилянгиз: ' + name + '\n Компаниянгиз номи: ' + company + '\n Лавозимингиз: ' + job + '\n Ойлик айланмангиз: ' + money + 
+        '\n Муаммоларингиз: ' + problem  + '\n Тадбирдан кутаётганингиз: ' + waitin  + '\n Телефон рақамингиз: ' + phoneNum)
+            # time.sleep(1)
+            # start(message)
+        else:   
+            user_dict.clear()
+            msg = bot.send_message(message.chat.id, 'Бекор қилинди,\nИсм-Фамилянгизни қайтадан киритинг')
+            bot.register_next_step_handler(message, process_name_step)
 
-bot.enable_save_next_step_handlers()
+        
+    except Exception as e:
+        bot.reply_to(message, 'oooops, кутилмаган қарор!')
+
+
+bot.enable_save_next_step_handlers(delay=2)
 
 bot.load_next_step_handlers()
 
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://dashboard.heroku.com/apps/alhikma") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
+bot.infinity_polling()
